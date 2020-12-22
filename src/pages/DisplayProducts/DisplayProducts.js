87557ac9cd6b5130840e4e007103bf8bc5779bf6
Ong Min Siang <ongminsiang@gmail.com>
2020-12-22 14:32:51 +0800

    added footer

--- src/pages/DisplayProducts/DisplayProducts.js
+++ src/pages/DisplayProducts/DisplayProducts.js
@@ -8,6 +8,13 @@ import Slider from 'react-slick'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Spinner from 'react-bootstrap/Spinner'
+import {BiMouse} from 'react-icons/bi'
+import {animateScroll as scroll} from 'react-scroll'
+import {SocialIcon} from 'react-social-icons'
+import lazada from '../../images/lazada1.png'
+import shopee from '../../images/shopee2.png'
+import {MdLocationOn} from 'react-icons/md'
+import {Link} from 'react-router-dom'
 import './DisplayProducts.css'
 
 
@@ -64,7 +71,7 @@ const DisplayProducts = () => {
 
     return (
         <>
-        <Container className="product-container">
+        <Container className="product-container pb-5">
             <Row className="justify-content-around">
                 <Col md={5} xs={12} className="product-image-slider">
                     <Slider {...settings} asNavFor={nav2} ref={slider => (setSlider1(slider))}>
@@ -110,6 +117,81 @@ const DisplayProducts = () => {
                         </div>
                     </div>
                 </Col>
+            </Row>
+        </Container>
+        <Container fluid className="footer">
+            <Row className="justify-content-around">
+                <Col lg={5} xs={12} className="mb-5">
+                    <h1 className="text">Want to know more about us?</h1>
+                    <p className="text">Contact us at :</p>
+                    <div>
+                        <Row>
+                            <Col xs={1} className="p-0">
+                                <SocialIcon network="whatsapp" bgColor="black" fgColor="white" url="https://wa.link/9b010m"></SocialIcon>
+                            </Col>
+                            <Col xs={10} className="pl-4">
+                                <p className="number">+60 16-528 3678</p>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={1} className="p-0">
+                                <SocialIcon url="mailto:aroindustrialparts@gmail.com" bgColor="black" fgColor="white"></SocialIcon>
+                            </Col>
+                            <Col xs={10} className="pl-4">
+                                <p className="number">aroindustrialparts@gmail.com</p>
+                            </Col>
+                            
+                        </Row>
+                        <Row>
+                            <Col xs={1} className="pl-2 pt-2">
+                                <MdLocationOn size={35} color="white" className="location"/>
+                            </Col>
+                            <Col xs={10} className="pl-4">
+                                <p className="number">Laluan Perusahaan Menglembu 10, Kawasan Perusahaan Menglembu, 31450 Ipoh, Perak (Opposite Puspakom)</p>
+                            </Col>
+                            
+                        </Row>
+                    </div>
+                </Col>
+                <Col lg={3} xs={12} className="mb-5 pt-4">
+                    <p className="product-text">Aroparts' Products</p>
+                    <div className="contact">
+                        <Link className="link" to="/category/1/products"><p>Boiler System & Boiler Accessories</p></Link>
+                        <Link className="link" to="/category/2/products"><p>Electronic & Electrical Accessories</p></Link>
+                        <Link className="link" to="/category/3/products"><p>Hydraulic System & Accessories</p></Link>
+                        <Link className="link" to="/category/4/products"><p>Solar System</p></Link>
+                        <Link className="link" to="/category/5/products"><p>Pneumatic System & Accessories</p></Link>
+                        <Link className="link" to="/category/6/products"><p>Gauge</p></Link>
+                    </div>
+                </Col>
+                <Col lg={3} xs={12} className="mb-5 pt-4">
+                    <Row className="mb-5">
+                        <Col>
+                            <p className="product-text">Visit Us At :</p>
+                            <div>
+                                <a href="https://www.lazada.com.my/shop/aro-industrial-parts/?spm=a2o4k.pdp.seller.1.5a20383aJe1XNh&itemId=1725962070&channelSource=pdp" target="blank"><img src={lazada} width="55" height="55"></img></a>
+                                <a href="https://shopee.com.my/aroindustrialparts" target="blank"><img src={shopee} width="55" height="55"></img></a>
+                                
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row className="mt-3">
+                        <Col>
+                            <p className="product-text">Connect with us :</p>
+                            <div>
+                                <SocialIcon network="instagram" url="https://www.instagram.com" fgColor="white" bgColor="black" />
+                                <SocialIcon url="https://www.facebook.com" network="facebook" fgColor="white" bgColor="black"/>
+                                
+                            </div>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            <Row className="text-center justify-content-center">
+                <div onClick={() => scroll.scrollToTop(0)} className="scroll-top">
+                    <p className="m-0">Back to Top</p>
+                    <BiMouse size={20}></BiMouse>
+                </div>
             </Row>
         </Container>
         </>
--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -11,6 +11,12 @@ import Slider from 'react-slick'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import {FiArrowLeftCircle, FiArrowRightCircle} from 'react-icons/fi'
+import {BiMouse} from 'react-icons/bi'
+import {animateScroll as scroll} from 'react-scroll'
+import {SocialIcon} from 'react-social-icons'
+import lazada from '../../images/lazada1.png'
+import shopee from '../../images/shopee2.png'
+import {MdLocationOn} from 'react-icons/md'
 import "./Products.css"
 
 const Product = () => {
@@ -30,7 +36,8 @@ const Product = () => {
     return (
         <>
         {products.length ?
-        <Container className="card-container">
+        <>
+        <Container className="card-container pb-5">
         <Row>
             {products.map((product) => {
                 return (
@@ -40,7 +47,7 @@ const Product = () => {
                             <img src={product.url} className="product-image"/> 
                         </div></Link>                                               
                         <Card.Body className="text-center">
-                        <Link to={`/products/${product.id}`} className="product-link"><Card.Title>{product.product_name}</Card.Title></Link>
+                        <Link to={`/products/${product.id}`} className="product-link"><Card.Title className="product-name">{product.product_name}</Card.Title></Link>
                         <Link to={`/products/${product.id}`} className="product-link"><Card.Title>{product.product_chinese_name}</Card.Title></Link>
                         </Card.Body>
                     </Card>
@@ -50,6 +57,83 @@ const Product = () => {
             
         </Row>
         </Container>
+        <Container fluid className="footer">
+            <Row className="justify-content-around">
+                <Col lg={5} xs={12} className="mb-5">
+                    <h1 className="text">Want to know more about us?</h1>
+                    <p className="text">Contact us at :</p>
+                    <div>
+                        <Row>
+                            <Col xs={1} className="p-0">
+                                <SocialIcon network="whatsapp" bgColor="black" fgColor="white" url="https://wa.link/9b010m"></SocialIcon>
+                            </Col>
+                            <Col xs={10} className="pl-4">
+                                <p className="number">+60 16-528 3678</p>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={1} className="p-0">
+                                <SocialIcon url="mailto:aroindustrialparts@gmail.com" bgColor="black" fgColor="white"></SocialIcon>
+                            </Col>
+                            <Col xs={10} className="pl-4">
+                                <p className="number">aroindustrialparts@gmail.com</p>
+                            </Col>
+                            
+                        </Row>
+                        <Row>
+                            <Col xs={1} className="pl-2 pt-2">
+                                <MdLocationOn size={35} color="white" className="location"/>
+                            </Col>
+                            <Col xs={10} className="pl-4">
+                                <p className="number">Laluan Perusahaan Menglembu 10, Kawasan Perusahaan Menglembu, 31450 Ipoh, Perak (Opposite Puspakom)</p>
+                            </Col>
+                            
+                        </Row>
+                    </div>
+                </Col>
+                <Col lg={3} xs={12} className="mb-5 pt-4">
+                    <p className="product-text">Aroparts' Products</p>
+                    <div className="contact">
+                        <Link className="link" to="/category/1/products"><p>Boiler System & Boiler Accessories</p></Link>
+                        <Link className="link" to="/category/2/products"><p>Electronic & Electrical Accessories</p></Link>
+                        <Link className="link" to="/category/3/products"><p>Hydraulic System & Accessories</p></Link>
+                        <Link className="link" to="/category/4/products"><p>Solar System</p></Link>
+                        <Link className="link" to="/category/5/products"><p>Pneumatic System & Accessories</p></Link>
+                        <Link className="link" to="/category/6/products"><p>Gauge</p></Link>
+                    </div>
+                </Col>
+                <Col lg={3} xs={12} className="mb-5 pt-4">
+                    <Row className="mb-5">
+                        <Col>
+                            <p className="product-text">Visit Us At :</p>
+                            <div>
+                                <a href="https://www.lazada.com.my/shop/aro-industrial-parts/?spm=a2o4k.pdp.seller.1.5a20383aJe1XNh&itemId=1725962070&channelSource=pdp" target="blank"><img src={lazada} width="55" height="55"></img></a>
+                                <a href="https://shopee.com.my/aroindustrialparts" target="blank"><img src={shopee} width="55" height="55"></img></a>
+                                
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row className="mt-3">
+                        <Col>
+                            <p className="product-text">Connect with us :</p>
+                            <div>
+                                <SocialIcon network="instagram" url="https://www.instagram.com" fgColor="white" bgColor="black" />
+                                <SocialIcon url="https://www.facebook.com" network="facebook" fgColor="white" bgColor="black"/>
+                                
+                            </div>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            <Row className="text-center justify-content-center">
+                <div onClick={() => scroll.scrollToTop(0)} className="scroll-top">
+                    <p className="m-0">Back to Top</p>
+                    <BiMouse size={20}></BiMouse>
+                </div>
+            </Row>
+        </Container>
+    </>
+
         :
         <div className="d-flex justify-content-center loading-head">
             <Spinner animation="border" role="status" className="loading-div">
